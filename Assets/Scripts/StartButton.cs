6cdace1d92bb349016c2b613dba4b17aa53efaca
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-12-11 16:40:54 +0000

    Added score counter and score
    Added collectables to the level that give different score depending on item and adds to the score

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This allows us to use the scene loading function
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    // This will be called by the button component
+    // when the button is clicked
+
+    public void StartGame()
+    {
+        // Reset score
+        PlayerPrefs.DeleteKey("score");
+
+        // Reset lives
+        PlayerPrefs.DeleteKey("lives");
+
+        // Load the first level
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+}
